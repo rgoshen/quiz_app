6892f068975f31f5a11c553281cab048299268a5
Richard Goshen <53133657+rgoshen@users.noreply.github.com>
2020-07-29 16:21:02 -0700

    Added local storage for high score

--- end.js
+++ end.js
@@ -2,16 +2,29 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore'); // localStorage stores as string
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []; //JSON.parse converts string into array
+
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-    console.log(username.value);
     saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
     console.log('clicked the save button');
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score); // sorted array
+    highScores.splice(5);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
 };
