959fc8f0eab1af73cfca2031cbf94f0afec55c38
Richard Goshen <53133657+rgoshen@users.noreply.github.com>
2020-07-29 20:35:33 -0700

    added questions from api

--- game.js
+++ game.js
@@ -10,13 +10,29 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
+// Get questions
 let questions = [];
-fetch('questions.json')
+fetch('https://opentdb.com/api.php?amount=10&type=multiple')
     .then(res => {
         return res.json();
     }).then(loadedQuestions => {
-        console.log(loadedQuestions);
-        questions = loadedQuestions;
+        console.log(loadedQuestions.results);
+        questions = loadedQuestions.results.map(loadedQuestion => {
+            const formattedQuestion = {
+                question: loadedQuestion.question
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+            answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+
         startGame();
     })
     .catch(err => {
