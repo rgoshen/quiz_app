96da0b151f1aee38077c179c86547ddc3a9cbc51
Richard Goshen <53133657+rgoshen@users.noreply.github.com>
2020-08-03 20:56:36 -0700

    fixed link

--- end.js
+++ end.js
@@ -26,5 +26,5 @@ saveHighScore = (e) => {
     highScores.sort((a, b) => b.score - a.score); // sorted array
     highScores.splice(5);
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('index.html');
 };
